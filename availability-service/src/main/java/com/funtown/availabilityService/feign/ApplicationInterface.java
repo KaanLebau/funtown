97fc4225f3918eeff0572a482e6c993e6d2458d9
r97-glitch <83668107+r97-glitch@users.noreply.github.com>
2024-03-07 19:00:10 +0100

    more renaming

---
+++ availability-service/src/main/java/com/funtown/availabilityService/feign/ApplicationInterface.java
@@ -0,0 +1,7 @@
+package com.funtown.availabilityService.feign;
+
+//import org.springframework.cloud.openfeign.FeignClient;
+
+//@FeignClient("USER-SERVICE")
+public interface ApplicationInterface {
+}
