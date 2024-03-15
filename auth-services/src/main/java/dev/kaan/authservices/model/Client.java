61aef9c06e97986b33d8dbd442ecf9d025a59011
Kaan Ozsan <75976200+KaanLebau@users.noreply.github.com>
2024-03-15 14:52:30 +0100

    encript password for old databse and populate the auth_service database

--- auth-services/src/main/java/dev/kaan/authservices/model/Client.java
+++ auth-services/src/main/java/dev/kaan/authservices/model/Client.java
@@ -38,7 +38,7 @@ import java.util.List;
 @Table(name = "client")
 public class Client implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
--- user-Service/src/main/resources/data.sql
+++ user-Service/src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO person (id ,first_name, last_name, pnr, email, username) VALUES (1, 'Kaan', 'ozsan', NULL, 'Joelle@Wilkinson.com', 'kaanozsan');
+SELECT setval('person_id_seq',2, false);
