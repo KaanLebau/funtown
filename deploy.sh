734fbd703c6a0338cb287bbcc9d71f5396851063
m7tar <91196060+m7tar@users.noreply.github.com>
2024-03-15 10:59:00 +0100

    added linux scripts for docker and deployment

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd client
+echo running npm install
+npm install
+echo building the application
+npm run build
+echo sending the build to the cloud
+scp -r build/* root@167.172.47.225:/var/www/167.172.47.225
